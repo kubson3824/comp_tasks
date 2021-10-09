bc4b99447cbf4e327fddf2913954350464d65cb6
kubson3824 <44587778+kubson3824@users.noreply.github.com>
2021-10-09 03:41:28 +0200

    Update Tree.java
    Removing unnecessary comment

--- src/main/java/com/kuba/comp/model/task1/Tree.java
+++ src/main/java/com/kuba/comp/model/task1/Tree.java
@@ -1,17 +1,5 @@
 package com.kuba.comp.model.task1;
 
-/*
-Zaimplementować w języku Java abstrakcję klas drzewa liściastego i iglastego (chodzi model OOP zwykłych drzew występujących w środowisku naturalnym) - każde drzewo ma pień, gałęzie i liście oraz metodę rośnij i itp.
-Zaimplementować bibliotekę kliencką obsługi faktur w systemie płatności Stripe (https://stripe.com/docs/api/invoices). W ramach zadania należy obsłużyć tworzenie i pobieranie faktur.
-Wymagania implementacyjne:
-
-opis rozwiązania w projekcie w pliku README.md,
-testy jednostkowe rozwiązania,
-obsługa logowania,
-walidacja parametrów,
-wykorzystanie spring boota 2.
- */
-
 import java.util.List;
 
 public abstract class Tree {
